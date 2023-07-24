24a183620014af4effe4e15b907efd6886e33b52
Juan Lesmes <juanlesmes22@hotmail.com>
2023-07-24 12:41:14 +0200

    - add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <header>
+                <p>
+                    Welcome to <code>frontend</code>!
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
